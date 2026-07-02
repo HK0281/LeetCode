fec34c9aceae4500ad94785ec06fb1d5e7be8535
unknown <hanzhic@usc.edu>
2026-07-02 23:48:05 +0800

    1137

---
+++ 1137.n-th-tribonacci-number.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1137 lang=python3
+#
+# [1137] N-th Tribonacci Number
+#
+
+# @lc code=start
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+# @lc code=end
+
