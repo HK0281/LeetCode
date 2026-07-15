8d2e0936afe5dcafd4e354defb4be86fc1336c4d
unknown <hanzhic@usc.edu>
2026-07-15 22:25:04 +0800

    1318

---
+++ 1318.minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1318 lang=python3
+#
+# [1318] Minimum Flips to Make a OR b Equal to c
+#
+
+# @lc code=start
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        
+# @lc code=end
+
